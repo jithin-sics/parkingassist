8e3bd3d791a2e4b711a4819a7a23d284b96d73d6
jithin <jithin.sics@gmail.com>
2023-11-02 10:22:37 +0530

    1st commit

---
+++ mainapp/views.py
@@ -0,0 +1,8 @@
+from django import contrib
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+# Create your views here.
+
+
+def Home(request):
+    return render(request,"base.html")
